b398619094dcd00cbae8157781354a71481c3556
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-11-21 20:18:28 -0500

    Add custom architecture handling to run.sh

---
+++ helpers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+architecture_prefix() {
+  arch=$(uname -m)
+  # Docker architecture naming differs from the output of uname -m
+  # See https://github.com/docker-library/official-images#architectures-other-than-amd64
+  case "$arch" in
+    "x86_64" )
+    # No prefix for x86_64
+    ;;
+    "armv7l" )
+    echo "arm32v7-"
+    ;;
+    * )
+    echo "Warning: Untested architecture: $arch" >&2
+    echo "Build is likely to FAIL" >&2
+    echo "$arch-"
+    ;;
+  esac
+}
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-
 cd "$(dirname "$0")"
+source helpers.sh
+
 if which nvidia-docker > /dev/null 2>&1 ; then
     DOCKER=nvidia-docker
 else
@@ -11,7 +12,7 @@ fi
 
 xhost +local:root
 CMD="$DOCKER run --rm --privileged --network host -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /dev/bus/usb -v /tmp/.X11-unix:/tmp/.X11-unix:ro"
-CONTAINER="thunderatz/ros-base"
+CONTAINER="thunderatz/$(architecture_prefix)ros-base"
 if (( $# == 0 )) ; then
     eval "$CMD $CONTAINER"
 else
