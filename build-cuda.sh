23b1959315aadb212c08345bb9b0c7ac2289477a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-06-24 15:52:44 -0700

    Create ros-base-cuda image fom nvidia/cuda:9.2-cudnn7-devel-ubuntu16.04

---
+++ build-cuda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+docker build -t "thunderatz/ros-base-cuda" --build-arg base_image=nvidia/cuda:9.2-cudnn7-devel-ubuntu16.04 docker/
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 cd "$(dirname "$0")"
 source helpers.sh
 image_prefix=$(architecture_prefix)
-docker build -t "thunderatz/${image_prefix}ros-base" --build-arg base_image_prefix=${image_prefix/-/\/} docker/
+docker build -t "thunderatz/${image_prefix}ros-base" --build-arg base_image=${image_prefix/-/\/}ubuntu:xenial docker/
